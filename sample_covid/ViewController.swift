4660e63aaa83917d5b8ab4453dc97a4b148fe487
ryuji <ryujispjp@gmail.com>
2021-12-14 21:28:32 +0900

    View系追加

--- sample_covid/ViewController.swift
+++ sample_covid/ViewController.swift
@@ -47,12 +47,52 @@ class ViewController: UIViewController {
         let leftX = view.frame.size.width * 0.33
         let rightX = view.frame.size.width * 0.80
 
+        setUpLabel("Covid in Japan", size: CGSize(width: 180, height: 35), centerX: view.center.x - 20, y: -60, font: .systemFont(ofSize: 25, weight: .heavy), color: .white, contentView)
         setUpLabel("PCR数", size: size, centerX: leftX, y: 20, font: labelFont, color: color, contentView)
         setUpLabel("感染者数", size: size, centerX: rightX, y: 20, font: labelFont, color: color, contentView)
         setUpLabel("入院者数", size: size, centerX: leftX, y: 120, font: labelFont, color: color, contentView)
         setUpLabel("重症者数", size: size, centerX: rightX, y: 120, font: labelFont, color: color, contentView)
         setUpLabel("死者数", size: size, centerX: leftX, y: 220, font: labelFont, color: color, contentView)
         setUpLabel("退院者数", size: size, centerX: rightX, y: 220, font: labelFont, color: color, contentView)
+
+        let height = view.frame.size.height / 2
+
+        setUpButton("健康管理", size: size, y: height + 190, color: colors.blue, parentView: view)
+        setUpButton("県別状況", size: size, y: height + 240, color: colors.blue, parentView: view)
+
+        setUpImageButton("chat", x: view.frame.size.width - 50).addTarget(self, action: #selector(chatAction), for: .touchDown)
+        setUpImageButton("reload", x: 10).addTarget(self, action: #selector(reloadAction), for: .touchDown)
+    }
+
+    func setUpImageButton(_ name: String, x: CGFloat) -> UIButton {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: name), for: .normal)
+        button.frame.size = CGSize(width: 30, height: 30)
+        button.tintColor = .white
+        button.frame.origin = CGPoint(x: x, y: 25)
+        view.addSubview(button)
+        return button
+    }
+
+    @objc func reloadAction() {
+        loadView()
+        viewDidLoad()
+    }
+
+    @objc func chatAction() {
+        print("タップchat")
+    }
+
+    func setUpButton(_ title: String, size: CGSize, y: CGFloat, color: UIColor, parentView: UIView) {
+        let button = UIButton(type: .system)
+        button.setTitle(title, for: .normal)
+        button.frame.size = size
+        button.center.x = view.center.x
+        let attributedTitle = NSAttributedString(string: title, attributes: [NSAttributedString.Key.kern: 8.0])
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.frame.origin.y = y
+        button.setTitleColor(color, for: .normal)
+        parentView.addSubview(button)
     }
 
     func setUpLabel(_ text: String, size: CGSize, centerX: CGFloat, y: CGFloat, font: UIFont, color: UIColor, _ parentView: UIView) {
